7037595b6dfad6213344949abf853a21c0789b7f
Yang Zhang <yang@users.noreply.github.com>
2021-08-18 00:47:13 -0700

    Add some code components

--- pages/[...catchall].tsx
+++ pages/[...catchall].tsx
@@ -1,8 +1,20 @@
-import * as React from 'react';
-import { PlasmicComponent, ComponentRenderData, PlasmicRootProvider, initPlasmicLoader } from '@plasmicapp/loader-nextjs';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import Error from 'next/error';
-import { PLASMIC } from '../plasmic-init';
+/** @format */
+
+import * as React from "react";
+import {
+  PlasmicComponent,
+  ComponentRenderData,
+  PlasmicRootProvider,
+  initPlasmicLoader,
+} from "@plasmicapp/loader-nextjs";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Error from "next/error";
+import { PLASMIC } from "../plasmic-init";
+import Iframe from "../components/Iframe";
+import { Reveal } from "../components/Reveal";
+
+PLASMIC.registerComponent(Iframe, "Iframe");
+PLASMIC.registerComponent(Reveal, "Reveal");
 
 /**
  * Use fetchPages() to fetch list of pages that have been created in Plasmic
@@ -11,9 +23,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const pages = await PLASMIC.fetchPages();
   return {
     paths: pages.map((page) => ({
-      params: { catchall: page.path.substring(1).split('/') }
+      params: { catchall: page.path.substring(1).split("/") },
     })),
-    fallback: false
+    fallback: false,
   };
 };
 
@@ -24,18 +36,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const { catchall } = context.params ?? {};
 
   // Convert the catchall param into a path string
-  const plasmicPath = typeof catchall === 'string' ? catchall : Array.isArray(catchall) ? `/${catchall.join('/')}` : '/';
+  const plasmicPath =
+    typeof catchall === "string"
+      ? catchall
+      : Array.isArray(catchall)
+      ? `/${catchall.join("/")}`
+      : "/";
   const plasmicData = await PLASMIC.maybeFetchComponentData(plasmicPath);
   if (plasmicData) {
     // This is a path that Plasmic knows about; pass the data
     // in as props
     return {
-      props: { plasmicData }
+      props: { plasmicData },
     };
   } else {
     // This is some non-Plasmic catch-all page
     return {
-      props: {}
+      props: {},
     };
   }
 };
@@ -43,7 +60,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
 /**
  * Actually render the page!
  */
-export default function CatchallPage(props: { plasmicData?: ComponentRenderData }) {
+export default function CatchallPage(props: {
+  plasmicData?: ComponentRenderData;
+}) {
   const { plasmicData } = props;
   if (!plasmicData || plasmicData.entryCompMetas.length === 0) {
     return <Error statusCode={404} />;
--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,8 +1,65 @@
 /** @format */
 
 import * as React from "react";
-import { PlasmicCanvasHost } from "@plasmicapp/host";
+import { PlasmicCanvasHost, registerComponent } from "@plasmicapp/host";
 import Head from "next/head";
+import Iframe from "../components/Iframe";
+import { Reveal } from "../components/Reveal";
+import { ParallaxWrapper } from "../components/ParallaxWrapper";
+
+registerComponent(Iframe, {
+  name: "Iframe",
+  props: {
+    src: "string",
+    loadWhileEditing: "boolean",
+  },
+  importPath: "./components/Iframe",
+});
+
+registerComponent(ParallaxWrapper, {
+  name: "Parallax",
+  props: {
+    x: "object",
+    y: "object",
+    disabled: "boolean",
+    children: "slot",
+  },
+  importPath: "./components/ParallaxWrapper",
+  importName: "ParallaxWrapper",
+});
+
+registerComponent(Reveal, {
+  name: "Reveal",
+  props: {
+    children: "slot",
+    effect: {
+      type: "choice",
+      options: [
+        "bounce",
+        "fade",
+        "flip",
+        "hinge",
+        "jackinthebox",
+        "roll",
+        "rotate",
+        "slide",
+        "zoom",
+      ],
+    },
+    cascade: "boolean",
+    damping: "number",
+    direction: {
+      type: "choice",
+      options: ["up", "down", "left", "right"],
+    },
+    delay: "number",
+    duration: "number",
+    fraction: "number",
+    triggerOnce: "boolean",
+  },
+  importPath: "./components/Reveal",
+  importName: "Reveal",
+});
 
 export default function Host() {
   return (
